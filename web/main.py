ee0e094cdfcf9b9ca9bce5a60e5a49975d9b8417
Philipp <github@philippeisen.de>
2016-04-12 17:06:28 +0200

    search with image basic web app first commit

---
+++ web/main.py
@@ -0,0 +1,71 @@
+import os
+import json
+import flask
+import requests
+from werkzeug.utils import secure_filename
+from irocr.meta import censure
+from irocr import config
+
+UPLOAD_FOLDER = '/root/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
+class SolrConfig(object):
+    def __init__(self):
+        cfg = config.get('solr')
+        if not cfg:
+            raise RuntimeError('Solr config not found')
+        assert 'host' in cfg
+        assert 'port' in cfg
+        self.host = cfg['host']
+        self.port = cfg['port']
+
+
+solar_config = SolrConfig()
+
+app = flask.Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def query_by_image():
+    if flask.request.method == 'POST':
+        file = flask.request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            fullpath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(fullpath)
+            keypoints = censure(fullpath)
+
+            url = 'http://{}:{}/solr/{}/query?echoParams=none'.format(
+                solar_config.host,
+                solar_config.port,
+                'photos'
+            )
+
+            json_query = {'query': '*.*', 'filter': []}
+            for x, y in keypoints:
+                json_query['filter'].append('keypoints.x:' + str(x))
+                json_query['filter'].append('keypoints.y:' + str(y))
+
+            response = requests.post(url, data=json.dumps(json_query), headers={'Content-type': 'application/json'})
+            return response.text
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Search for image</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Search>
+    </form>
+    '''
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
