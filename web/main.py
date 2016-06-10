46476a847605d7d36deda5eb27d282eaa9e04d9a
Philipp <github@philippeisen.de>
2016-06-10 11:05:01 +0200

    don't echo params

--- web/main.py
+++ web/main.py
@@ -48,7 +48,7 @@ def query_by_image():
                 'photos'
             )
 
-            json_query = {'query': '*:*', 'filter': []}
+            json_query = {'query': 'keypoints.x:*', 'filter': []}
             for x, y in keypoints:
                 json_query['filter'].append('keypoints.x:' + str(x))
                 json_query['filter'].append('keypoints.y:' + str(y))
